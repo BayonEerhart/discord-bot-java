24494883b59ed365f57c65804f53ea52beae9594
Bayon <bayoneerhart758@gmail.com>
2023-11-08 08:56:20 +0100

    done

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+
+
+package org.example;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws IOException, ParseException {
+        Object o = new JSONParser().parse(new FileReader("settings.json"));
+        JSONObject j = (JSONObject) o;
+
+        String token = (String) j.get("token");
+
+        try {
+            JDA jda = JDABuilder.createDefault(token)
+                    .addEventListeners(new Main())
+                    .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                    .build();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+//        System.out.println(event.getMessage());
+//        System.out.println(event.getMessage().getAuthor().getId());
+
+        if (!event.getAuthor().isBot() && event.getMessage().getContentRaw().trim().startsWith("&", 0)) {
+            String response = Response.getResponse(event.getMessage().getContentRaw(), event.getMessage().getAuthor().getId());
+            if (response != null) {
+                event.getChannel().sendMessage(response).queue();
+            }
+        }
+    }
+}
---
+++ src/main/java/org/example/Response.java
@@ -0,0 +1,86 @@
+package org.example;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import com.github.dhiraj072.randomwordgenerator.RandomWordGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.*;
+import java.util.Random;
+
+
+public class Response {
+    public static String getResponse(String event, String id) {
+
+        switch ((event.split(" "))[0]) {
+            case "&hello":
+                return "hello, there";
+
+            case "&bye":
+                return "cya";
+
+            case "&time":
+                String hour = String.valueOf(ZonedDateTime.now(ZoneId.of("Europe/Amsterdam")).getHour());
+                String minute = String.valueOf(ZonedDateTime.now(ZoneId.of("Europe/Amsterdam")).getMinute());
+                String seconds = String.valueOf(ZonedDateTime.now(ZoneId.of("Europe/Amsterdam")).getSecond());
+                return ("time: " + LocalTime.of(Integer.parseInt(hour), Integer.parseInt(minute), Integer.parseInt(seconds)));
+
+            case "&kill":
+                Object o = null;
+                try {
+                    o = new JSONParser().parse(new FileReader("settings.json"));
+                } catch (IOException | ParseException e) {
+                    throw new RuntimeException(e);
+                }
+                JSONObject j = (JSONObject) o;
+                List mods_id = (List) j.get("mods");
+                if (mods_id.contains(id)) {
+                    System.exit(0);
+                } else {
+                    return "```diff\n" +
+                            "- you need mod to use this command\n" +
+                            "```";
+                }
+            case "&word":
+                return RandomWordGenerator.getRandomWord();
+            case "&new":
+                if (event.split(" ").length <= 2){
+                    return "try &new @<someone> <zone>";
+                }
+                if (event.split(" ")[1].startsWith("@", 1)) {
+                    System.out.println("works to so far");
+
+                    File jsonFile = new File("user_data.json");
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    try {
+                        JsonNode rootNode = objectMapper.readTree(jsonFile);
+                        if (rootNode.isObject()) {
+                            ObjectNode objectNode = (ObjectNode) rootNode;
+                            objectNode.put(event.split(" ")[1], event.split(" ")[2]);
+                            objectMapper.writeValue(jsonFile, rootNode);
+                            System.out.println("done");
+                        } else {
+                            System.err.println("The JSON data in the file is not an object.");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(event);
+
+
+            default:
+                return null;
+        }
+    }
+}
